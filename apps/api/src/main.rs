f139bef2a64833c7bdc967bcd34043b22a16d923
BennyyyBB <bennybb028@gmail.com>
2026-06-09 18:17:47 +0200

    undo former changes and removed added file

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -1,7 +1,3 @@
-use emote_copy::seventv_router;
-	let app = Router::new()
-		.merge(seventv_router());
-
 use global::Global;
 use scuffle_bootstrap_telemetry::TelemetrySvc;
 use scuffle_signal::SignalSvc;
@@ -10,7 +6,6 @@ mod config;
 mod connections;
 mod cron;
 mod dataloader;
-mod emote_copy;
 mod global;
 mod http;
 mod image_processor;
